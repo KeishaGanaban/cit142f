2bec69f6b484203fca14f6a214e8d7570d3fe1ed
CIT142F-1@m307.uc-bcf.edu.ph <CIT142F-1@m307.uc-bcf.edu.ph>
2022-09-14 17:34:16 +0800

    -batch upload

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
-git remote set-url origin https://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.com/KeishaGanaban/cit142f.git
+git remote set-url origin https://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@github.com/KeishaGanaban/cit142f.git
 pause
 git add .
 git commit -m -"batch upload"
